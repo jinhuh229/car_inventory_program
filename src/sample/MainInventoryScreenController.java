976a71bd2ce676ad547ba6689ce20f195920eb05
Jin Huh <37564888+jinhuh229@users.noreply.github.com>
2019-05-06 11:10:20 -0400

    Add files via upload

--- src/sample/MainInventoryScreenController.java
+++ src/sample/MainInventoryScreenController.java
@@ -51,6 +51,26 @@ public class MainInventoryScreenController implements Initializable {
     @FXML
     private TableColumn<car, Integer>  id_table;
 
+
+    // -----------------------  searching table --------------------------------------
+    @FXML
+    private TableView<car> search_table;
+
+    @FXML
+    private TableColumn<car, String>  model_table_search;
+
+    @FXML
+    private TableColumn<car, Integer>  year_table_search;
+
+    @FXML
+    private TableColumn<car, Integer>  mileage_table_search;
+
+    @FXML
+    private TableColumn<car, String>  color_table_search;
+
+    @FXML
+    private TableColumn<car, Integer>  id_table_search;
+
     @FXML
     Button deleteItem;
 
@@ -85,6 +105,7 @@ public class MainInventoryScreenController implements Initializable {
 
 
     ObservableList<car> oblist = FXCollections.observableArrayList();
+    ObservableList<car> oblist1 = FXCollections.observableArrayList();
 
     @Override
     public void initialize(URL location, ResourceBundle resources ) {
@@ -115,15 +136,17 @@ public class MainInventoryScreenController implements Initializable {
         productTable.setItems(oblist);
     }
 
-
+    // --------------------------- Refresh the list on the table after add or delete  ----------------------------
     @FXML
     public void refesh(){
 
         oblist.clear();
+        oblist1.clear();
         loadDataBase();
     }
 
 
+    // --------------------------- show the item from the list & database on search part ----------------------------
     @FXML
     public void showColumnTable() throws SQLException{
         PreparedStatement preparedStatement=null;
@@ -135,10 +158,10 @@ public class MainInventoryScreenController implements Initializable {
             Connection con = DBConnection.getConnection();
             preparedStatement = con.prepareStatement(query);
 
-            model_search.setText(car.model);
-            year_search.setText(String.valueOf(car.year));
-            mileage_search.setText(String.valueOf(car.mileage));
-            color_search.setText(car.color);
+            model_search.setText(car.getModel());
+            year_search.setText(String.valueOf(car.getYear()));
+            mileage_search.setText(String.valueOf(car.getMileage()));
+            color_search.setText(car.getColor());
 
             preparedStatement.close();
 
@@ -152,6 +175,102 @@ public class MainInventoryScreenController implements Initializable {
     }
 
 
+    // --------------------------- Delete a item from the list & database ----------------------------
+    @FXML
+    public void deleteColumnTable() throws SQLException{
+
+        PreparedStatement preparedStatement=null;
+
+        try {
+            car car =(car)productTable.getSelectionModel().getSelectedItem();
+            String query = "DELETE FROM car WHERE model=?";
+            Connection con = DBConnection.getConnection();
+            preparedStatement = con.prepareStatement(query);
+            preparedStatement.setString(1, car.getModel());
+            preparedStatement.executeUpdate();
+
+            preparedStatement.close();
+
+            Alert alert = new Alert(AlertType.CONFIRMATION);
+            alert.setTitle("Confirmation Dialog");
+            alert.setHeaderText("Look, a Confirmation Dialog");
+            alert.setContentText("Delete Complete");
+
+            refesh();
+
+
+
+        } catch (SQLException ex){
+            ex.printStackTrace();
+        }
+
+
+    }
+
+
+    // --------------------------- Reset a item from the searching boxes ----------------------------
+    @FXML
+    public void resetButton() {
+
+        model_search.clear();
+        year_search.clear();
+        mileage_search.clear();
+        color_search.clear();
+
+
+    }
+
+
+
+    // --------------------------- Search the items ----------------------------
+    @FXML
+    public void Search_items() throws SQLException{
+
+
+
+            PreparedStatement preparedStatement=null;
+
+            try {
+
+                String query = "SELECT * FROM car WHERE model LIKE '%" + model_search.getText() + "%'";
+                Connection con = DBConnection.getConnection();
+                preparedStatement = con.prepareStatement(query);
+
+                //preparedStatement.setString(1, model_search.getText());
+
+                ResultSet rs = preparedStatement.executeQuery();
+
+                if(rs.next()){
+                    oblist1.add(new car(rs.getString("model"), rs.getInt("year"),rs.getInt("mileage"),rs.getString("color"),rs.getInt("id")));
+
+
+
+                }
+
+                model_table_search.setCellValueFactory(new PropertyValueFactory<>("model"));
+                year_table_search.setCellValueFactory(new PropertyValueFactory<>("year"));
+                mileage_table_search.setCellValueFactory(new PropertyValueFactory<>("mileage"));
+                color_table_search.setCellValueFactory(new PropertyValueFactory<>("color"));
+                id_table_search.setCellValueFactory(new PropertyValueFactory<>("id"));
+
+
+                for(int i=0; i<oblist1.size(); i++) {
+                    search_table.setItems(oblist1);
+                }
+                //preparedStatement.close();
+
+
+
+
+
+            } catch (SQLException ex){
+                ex.printStackTrace();
+            }
+
+
+    }
 }
+
+
 
 
