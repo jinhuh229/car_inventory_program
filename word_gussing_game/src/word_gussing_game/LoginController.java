19a70e89bdbf8bdca2f2365b79936b69bd2589c2
Jin Huh <37564888+jinhuh229@users.noreply.github.com>
2019-05-08 23:30:00 -0400

    Add files via upload

---
+++ word_gussing_game/src/word_gussing_game/LoginController.java
@@ -0,0 +1,39 @@
+package word_gussing_game;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+
+public class LoginController {
+
+    @FXML
+    Button login_button;
+
+    @FXML
+    TextField username_text;
+
+    @FXML
+    PasswordField password_text;
+
+    @FXML
+    Label lost_password_text;
+
+    @FXML
+    Label no_account_text;
+
+    @FXML
+    Label exit_text;
+
+
+
+    public void login_clicked(){
+
+    }
+}
+
+
+
